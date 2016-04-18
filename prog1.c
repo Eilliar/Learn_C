b1e4999fcb036959cc30c7af924a23f90d90a1f1
BrunoEilliar <lord.eilliar@hotmail.com>
2016-04-18 08:30:04 -0300

    First Commit. First program.

---
+++ prog1.c
@@ -0,0 +1,15 @@
+/*
+First Program
+Author:
+	Bruno Godoi Eilliar
+Notes:
+	-To compile run gcc prog1.c -o prog1
+*/
+#include <stdio.h>
+
+int main (void)
+{
+	printf("Programming is fun.\n");
+
+	return 0;
+}
