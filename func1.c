3846d99bb04d5b7a3c2d506d38dc95cc15384688
BrunoEilliar <lord.eilliar@hotmail.com>
2016-04-18 08:38:23 -0300

    Add first example using function.

---
+++ func1.c
@@ -0,0 +1,23 @@
+/*
+Function usage example 1.
+Author:
+	Bruno Godoi Eilliar
+Date:
+	April 18, 2016.
+Notes:
+	-To compile run gcc func1.c -o func1
+*/
+
+#include <stdio.h>
+
+void printMessage(void)
+{
+	printf("Programming is fun.\n");
+}
+
+int main (void)
+{
+	printMessage();
+
+	return 0;
+}
